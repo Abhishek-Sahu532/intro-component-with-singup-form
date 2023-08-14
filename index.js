6c31936bca42d4720dc97500d226b5fc43b30612
Abhishek Sahu <asahu532@gmail.com>
2023-08-14 16:29:56 +0530

    Task completed

--- index.js
+++ index.js
@@ -5,15 +5,24 @@ submitBtn.addEventListener('click', (e) => {
     let firstName = document.getElementById('firstName').value;
     let secondName = document.getElementById('secondName').value
     let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+    let password = document.getElementById('pass').value
     let emailError = document.querySelector(".emailError");
 
+
+if(firstName == null || secondName == null || email == null || password == null ){
+    emailError.innerHTML = 'Provide the required details.'
+        emailError.style.display = "block";
+        return
+}
+
+
     if (emailFormat.test(email)) {
-        emailError.innerHTML = 'Please provide the valid email.<'
+        emailError.innerHTML = 'Form submitted.'
         emailError.style.display = "block";
     } else {
-        emailError.innerHTML = 'Please provide the valid email.<'
+        emailError.innerHTML = 'Please provide the valid email.'
         emailError.style.display = "block";
     }
+
 
 })
